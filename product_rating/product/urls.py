7626feb99545411fc4c2e98e5a8b777ac8553c53
Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>
2024-09-08 11:10:30 +0530

    Delete product_rating/product directory

--- product_rating/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('signup/', views.signup_view, name='signup'),
-]
