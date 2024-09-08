10ae3753bb4ac1b756f80a0f0b30f4f438f77362
Abhishek Kumar <ak0188644@gmail.com>
2024-09-08 10:08:46 +0530

    Revert "initial static folder added"
    This reverts commit c21fc2900402afe44f163e5be680086f1500d625.

--- product_rating/product/urls.py
+++ product_rating/product/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:product_rating/product/urls.py
 from django.urls import path
 from . import views
 
@@ -8,17 +7,3 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup_view, name='signup'),
 ]
-=======
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('signup/', views.signup_view, name='signup'),
-    path('result/', views.result, name='result'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
->>>>>>> Stashed changes:product/urls.py
