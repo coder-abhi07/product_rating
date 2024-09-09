cc9c313bceb567d1b77ac0eb2c0d1ee3aef9b403
Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>
2024-09-10 01:55:30 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,5 @@
-# build_files.sh
-
-sudo apt update
-sudo apt install -y software-properties-common
-
-# Add the deadsnakes PPA which contains Python 3.10
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-
-# Install Python 3.10 and pip
-sudo apt install -y python3.10 python3.10-venv python3.10-dev
-sudo apt install -y python3-pip
-
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/usr/bin/env bash
+python3 -m pip install requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
