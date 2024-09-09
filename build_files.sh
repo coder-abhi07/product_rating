712d7bb073fbd04a3f78a958ee8775f73f210694
Abhishek Kumar <ak0188644@gmail.com>
2024-09-10 01:59:46 +0530

    pip

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/usr/bin/env bash
+python3 -m pip install requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
