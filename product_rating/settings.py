b8635ab3a606440effd20b267a5ddb22e5bf37ca
Abhishek Kumar <ak0188644@gmail.com>
2024-09-12 22:36:24 +0530

    debug = False

--- product_rating/settings.py
+++ product_rating/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "product", "templates")
 SECRET_KEY = 'django-insecure-qv1mjbo_=e&$j0t^w+j0*hx8%o3!m^kchoe-nbdd$niabb7(l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
