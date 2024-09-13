3994ce5f9b0ed95e03608467d063d1e5104dff60
Abhishek Kumar <ak0188644@gmail.com>
2024-09-13 23:35:42 +0530

    added css and bug fixes

--- product/forms.py
+++ product/forms.py
@@ -1,47 +1,4 @@
-# # from django import forms
-# # from django.contrib.auth.models import User
-
-# # class UserUpdateForm(forms.ModelForm):
-# #     class Meta:
-# #         model = User
-# #         fields = ['username', 'first_name', 'last_name', 'email']
-
-# # from django import forms
-# # from .models import IngredientReview
-
-# # class IngredientReviewForm(forms.ModelForm):
-# #     class Meta:
-# #         model = IngredientReview
-# #         fields = ['rating', 'review_text']
-
-
-# from django import forms
 from django.contrib.auth.models import User
-# from .models import IngredientReview
-
-# class IngredientReviewForm(forms.ModelForm):
-#     class Meta:
-#         model = IngredientReview
-#         fields = ['rating', 'review_text']
-
-# class UserUpdateForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'first_name', 'last_name', 'email']
-
-# from .models import ProductRating
-# # forms.py
-# from django import forms
-# from .models import Review
-
-# class ReviewForm(forms.ModelForm):
-#     class Meta:
-#         model = Review
-#         fields = ['product', 'review_text']  # Specify the fields to include in the form
-
-#     # Optionally add custom validation here
-
-# forms.py
 from django import forms
 from .models import IngredientReview
 
--- product_rating/settings.py
+++ product_rating/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 
@@ -149,14 +149,14 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
   # Adjust this if your development static files are in a different directory
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'product/static'),
-    os.path.join(BASE_DIR,'static'),
+    
 ]
 
 
@@ -173,13 +173,13 @@ if ENV_FILE:
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
 AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
-# settings.py
+# # settings.py
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL = '/'  # Redirect to the home page after login
@@ -188,7 +188,6 @@ LOGIN_URL = '/login/'  # URL for the login page
 
 
 
-LOGIN_URL = '/login/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
